813293dbc3cd6012eff8f705c38a8a166a3070cc
edros <eduardrostomyan@gmail.com>
2020-04-19 07:23:22 -0700

    Adding leetcode

---
+++ Threads/LeetCode.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <iostream>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+
+/*
+Given a binary tree, return the vertical order traversal of its nodes values.
+*/
+class VerticalTraversal {
+	int left, right;
+public:
+	vector<vector<int>> verticalTraversal(TreeNode* root) {
+		unordered_map<int, set < pair<int, int>>> H;
+		traverse(root, H, 0, 0);
+		vector<vector<int>> res;
+		for (int i = left; i <= right; ++i)
+		{
+			auto v = H[i];
+			vector<int> curr;
+			for (auto it : v) { curr.push_back(it.second); }
+			res.push_back(curr);
+		}
+		return res;
+	}
+
+	template<class T>
+	void traverse(TreeNode* root, T& H, int level, int h)
+	{
+		if (root)
+		{
+			left = min(left, level);
+			right = max(right, level);
+			H[level].insert({ h,root->val });
+			traverse(root->left, H, level - 1, h + 1);
+			traverse(root->right, H, level + 1, h + 1);
+		}
+	}
+};
+
+
+/*
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+*/
+class MergeKLists {
+public:
+	ListNode* mergeKLists(vector<ListNode*>& lists) {
+		if (lists.empty()) { return NULL; }
+		ListNode* res = lists[0];
+		for (int i = 1; i < lists.size(); ++i)
+		{
+			res = mergeTwoLists(res, lists[i]);
+		}
+		return res;
+	}
+
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		if (!l1) { return l2; }
+		if (!l2) { return l1; }
+		if (l1->val <= l2->val)
+		{
+			l1->next = mergeTwoLists(l1->next, l2); return l1;
+		}
+		else
+		{
+			l2->next = mergeTwoLists(l1, l2->next); return l2;
+		}
+	}
+};
--- Threads/main.cpp
+++ Threads/main.cpp
@@ -1,8 +1,7 @@
-#include "Experimental.h"
+#include "LeetCode.h"
 
 int main()
 {
-	std::future<void> f = std::async(std::launch::deferred | std::launch::async, e);
-//	f.get();
+
 	return 0;
 }
