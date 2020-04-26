52d790226b4b7ec5886af41a0982268e8884e7d7
edros <eduardrostomyan@gmail.com>
2020-04-26 16:39:13 -0700

    Reorganise solution

--- Threads/main.cpp
+++ Threads/main.cpp
@@ -1,9 +1,24 @@
 #include "LeetCode.h"
 
+class A {
+	vector<map<string, string>> v;
+};
+
+class B: public A{
+	//virtual void f() {}
+};
+
+class C : public A {
+	//virtual void f() {}
+};
+
+class D :public C, public B{
+
+};
+
 int main()
 {
-	int t; cin >> t;
-	while (t--) {
-	}
+	D d;
+	C c;
 	return 0;
 }
