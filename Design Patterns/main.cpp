a832efae4a33b48e58cdb95e5fc1bee2438e94d0
edros <eduardrostomyan@gmail.com>
2020-05-09 04:23:15 -0700

    kthlargest

--- Design Patterns/main.cpp
+++ Design Patterns/main.cpp
@@ -15,7 +15,19 @@ public:
 
 using namespace html;
 
+template<int N>
+class factorial {
+public:
+	static const int result = N * factorial<N - 1>::result;
+};
+
+template<>
+class factorial<0> {
+public:
+	static const int result = 1;
+};
+
 int main() {
-	BuilderFacets::Person p = BuilderFacets::Person::create().lives().at("somewhere").withPostCode("e149ja").in("London").works().at("GS").asA("VP");
-	int a;
+	int a = factorial<5>::result;
+	std::cout << a << std::endl;
 }
--- Threads/LeetCode.h
+++ Threads/LeetCode.h
@@ -431,6 +431,11 @@ public:
 	}
 };
 
+
+/*M
+Find the kth largest element in an unsorted array.
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+*/
 class LongestIncreasinPath {
 public:
 	int dx[4] = { 0, -1, 1, 0 };
@@ -462,4 +467,42 @@ public:
 		}
 		return ans;
 	}
+
+	class KthLargestElement {
+	public:
+		int findKthLargest(vector<int>& nums, int k) {
+			int left = 0, right = nums.size() - 1, kth;
+			while (true) {
+				int idx = partition(nums, left, right);
+				if (idx == k - 1) {
+					kth = nums[idx];
+					break;
+				}
+				if (idx < k - 1) {
+					left = idx + 1;
+				}
+				else {
+					right = idx - 1;
+				}
+			}
+			return kth;
+		}
+	private:
+		int partition(vector<int>& nums, int left, int right) {
+			int pivot = nums[left], l = left + 1, r = right;
+			while (l <= r) {
+				if (nums[l] < pivot && nums[r] > pivot) {
+					swap(nums[l++], nums[r--]);
+				}
+				if (nums[l] >= pivot) {
+					l++;
+				}
+				if (nums[r] <= pivot) {
+					r--;
+				}
+			}
+			swap(nums[left], nums[r]);
+			return r;
+		}
+	};
 };
