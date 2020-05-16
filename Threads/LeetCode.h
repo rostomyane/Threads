41801ba71ec20b5e0a0500ef4f0510203bb982de
edros <eduardrostomyan@gmail.com>
2020-05-16 14:24:54 -0700

    add problems

--- Threads/LeetCode.h
+++ Threads/LeetCode.h
@@ -613,3 +613,42 @@ public:
 		return result;
 	}
 };
+
+
+/*M
+Copy single linke list with randon link
+*/
+class CopyRandomList {
+	struct Node {
+	public:
+		int val;
+		Node* next;
+		Node* random;
+		Node(int val) {
+
+		}
+	};
+public:
+	Node* copyRandomList(Node* head) {
+		Node* newHead, * l1, * l2;
+		if (head == NULL) return NULL;
+		for (l1 = head; l1 != NULL; l1 = l1->next->next) {
+			l2 = new Node(l1->val);
+			l2->next = l1->next;
+			l1->next = l2;
+		}
+
+		newHead = head->next;
+		for (l1 = head; l1 != NULL; l1 = l1->next->next) {
+			if (l1->random != NULL) l1->next->random = l1->random->next;
+		}
+
+		for (l1 = head; l1 != NULL; l1 = l1->next) {
+			l2 = l1->next;
+			l1->next = l2->next;
+			if (l2->next != NULL) l2->next = l2->next->next;
+		}
+
+		return newHead;
+	}
+};
