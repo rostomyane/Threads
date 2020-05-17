5d9f8e1bc9ac36a8b852890e6cf92b8c9bf42a61
edros <eduardrostomyan@gmail.com>
2020-05-17 14:09:25 -0700

    adding problems

--- Threads/LeetCode.h
+++ Threads/LeetCode.h
@@ -938,3 +938,20 @@ public:
 	}
 };
 
+/*
+Simplify Unix path
+*/
+class Simplify {
+	string simplifyPath(string path) {
+		string res, tmp;
+		vector<string> stk;
+		stringstream ss(path);
+		while (getline(ss, tmp, '/')) {
+			if (tmp == "" or tmp == ".") continue;
+			if (tmp == ".." and !stk.empty()) stk.pop_back();
+			else if (tmp != "..") stk.push_back(tmp);
+		}
+		for (auto str : stk) res += "/" + str;
+		return res.empty() ? "/" : res;
+	}
+};
