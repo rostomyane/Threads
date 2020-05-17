56dbfbc76d96a48f3eb2be959db2ece4a41b6970
edros <eduardrostomyan@gmail.com>
2020-05-17 09:24:17 -0700

    added problems

--- Algorithms/SystemDesign.h
+++ Algorithms/SystemDesign.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 /*
 Reliability is the probability a system will fail in a given period.
 In simple terms, a distributed system is considered reliable if it keeps delivering
@@ -16,4 +16,14 @@ Size of messages representing the volume of data exchanges.
 
  Load Balancer (LB) is another critical component of any distributed system. 
  It helps to spread the traffic across a cluster of servers to improve responsiveness and availability of services
+
+
+Times when you felt a huge sense of accomplishment or success.
+Times when you fixed something that wasn’t working.
+A time when you had to make a challenging decision.
+A time when you had to influence a leader or team.
+Times when you got surprised by something or had overlooked something.
+Times when you removed barriers or came up with a creative solution.
+A time when you failed but learned.
+A time when you had to defuse​ an escalating situation.
 */
--- Threads/LeetCode.h
+++ Threads/LeetCode.h
@@ -676,3 +676,89 @@ public:
 		return dp[m][n];
 	}
 };
+
+
+/*H
+Robot clean room
+*/
+class CleanRoom {
+public:
+	unordered_map<int, unordered_map<int, int>> data;
+	int x = 0;
+	int y = 0;
+	int dx[4] = { 1, 0, -1, 0 };
+	int dy[4] = { 0, 1, 0, -1 };
+	int dir = 0;
+	void cleanRoom(Robot& robot) {
+		if (data[x][y] == 1) {
+			return;
+		}
+		data[x][y] = 1;
+		robot.clean();
+		for (int i = 0; i < 4; i++) {
+			if (robot.move()) {
+				x += dx[dir];
+				y += dy[dir];
+				cleanRoom(robot);
+				robot.turnRight();
+				robot.turnRight();
+				robot.move();
+				robot.turnRight();
+				robot.turnRight();
+				x -= dx[dir];
+				y -= dy[dir];
+			}
+			robot.turnRight();
+			dir = (dir + 1) % 4;
+		}
+	}
+};
+
+
+/*
+Is a valid number? ex 2e10
+*/
+class IsNumber {
+private:
+	bool isSpace(char c) { return c == ' '; }
+	bool isSgn(char c) { return c == '+' || c == '-'; }
+	bool isDot(char c) { return c == '.'; }
+	bool isNum(char c) { return c <= '9' && c >= '0'; }
+	bool isE(char c) { return c == 'e' || c == 'E'; }
+
+public:
+	bool isNumber(string s) {
+		int pos = 0;
+		bool haveNum = false;
+
+		// Check all the prefix spaces
+		while (pos < s.size() && isSpace(s[pos])) pos++;
+
+		// Check the next char if it's a +/- signal
+		if (pos < s.size() && isSgn(s[pos])) pos++;
+
+		// Check the numbers before a dot '.'
+		while (pos < s.size() && isNum(s[pos])) { haveNum = true; pos++; }
+
+		// Check the dot '.'
+		if (pos < s.size() && isDot(s[pos])) pos++;
+
+		// Check the numbers after a dot '.'
+		while (pos < s.size() && isNum(s[pos])) { haveNum = true; pos++; }
+
+		// Check the 'e' / 'E'
+		if (haveNum && pos < s.size() && isE(s[pos])) {
+			haveNum = false; pos++;
+			if (pos < s.size() && isSgn(s[pos])) pos++;
+		}
+
+		// Check the numbers after 'e' / 'E'
+		while (pos < s.size() && isNum(s[pos])) { haveNum = true; pos++; }
+
+		// Check all the remaining spaces
+		while (pos < s.size() && isSpace(s[pos])) pos++;
+
+		// Everything is done, if the string not empty, return false.
+		return (pos == s.size() && haveNum);
+	}
+};
