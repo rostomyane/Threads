5d8c2ff807d618343aa4598e696470aaee7299ea
edros <eduardrostomyan@gmail.com>
2020-05-18 02:25:22 -0700

    fininsh

--- Threads/LeetCode.h
+++ Threads/LeetCode.h
@@ -1056,3 +1056,104 @@ protected:
 		return mp[n];
 	}
 };
+
+
+
+/*
+LRU cache
+*/
+class LRUCache {
+	struct Cache {
+		int _key;
+		int _value;
+		Cache(int key = 0, int value = 0) {
+			_key = key;
+			_value = value;
+		}
+	};
+
+	list<Cache> cache;
+	unordered_map<int, list<Cache>::iterator> posMap;
+	int _capacity;
+public:
+	LRUCache(int capacity) {
+		_capacity = capacity;
+	}
+
+	int get(int key) {
+		if (posMap.find(key) == posMap.end()) return -1;
+		//find the position of key
+		auto it = posMap[key];
+		//move the founded cache value to the front
+		cache.push_front(*it);
+		//delete the duplicate copy at the original position
+		cache.erase(it);
+		//update the posMap of key to new position: begin()
+		posMap[key] = cache.begin();
+		return posMap[key]->_value;
+	}
+
+	void set(int key, int value) {
+		//if cannot find the new key in the cache, need to insert it
+		//only in this case delete is needed, when cache size is larger than capacity
+		//else just update the value and update the cache key position to the front
+		if (posMap.find(key) == posMap.end()) {
+			//insert the new cache value to the front
+			cache.push_front(Cache(key, value));
+			posMap[key] = cache.begin();
+			//if size larger than capacity, delete the last value
+			if (cache.size() > _capacity) {
+				//delete the last elements' iterator in map, and delete the last element
+				Cache lastNode = cache.back();
+				posMap.erase(lastNode._key);
+				cache.pop_back();
+			}
+		}
+		else {
+			//find the position of the key, update value and update pos to begin()
+			auto it = posMap[key];
+			it->_value = value;
+			cache.push_front(*it);
+			cache.erase(it);
+			posMap[key] = cache.begin();
+		}
+	}
+};
+
+
+/*
+Insert,delete,getrand O(1)
+*/
+class RandomizedSet {
+public:
+	/** Initialize your data structure here. */
+	RandomizedSet() {
+
+	}
+
+	/** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+	bool insert(int val) {
+		if (m.count(val)) { return false; }
+		nums.push_back(val);
+		m[val] = nums.size() - 1;
+		return true;
+	}
+
+	/** Removes a value from the set. Returns true if the set contained the specified element. */
+	bool remove(int val) {
+		if (!m.count(val)) { return false; }
+		int last = nums.back();
+		m[last] = m[val];
+		nums[m[val]] = last;
+		m.erase(val);
+		nums.pop_back();
+		return true;
+	}
+
+	/** Get a random element from the set. */
+	int getRandom() {
+		return nums[rand() % nums.size()];
+	}
+	std::unordered_map<int, int> m;
+	vector<int> nums;
+};
