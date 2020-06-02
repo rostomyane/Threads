96a4c6913c34445c3dd496a4e75c832851056e89
EduardR <eduardrostomyan@gmail.com>
2020-06-02 03:23:59 -0700

    templates

--- Template Metaprogramming/Experimental.h
+++ Template Metaprogramming/Experimental.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <type_traits>
 #include <utility>
+#include <string>
 
 namespace experimental {
 
@@ -16,4 +17,27 @@ namespace experimental {
         return std::pair<std::decay_t<T1>, std::decay_t<T2>> 
             (std::forward<T1>(x), std::forward<T2>(y));
     }
+
+	class Customer {
+		std::string first;
+		std::string second;
+		int id;
+	public:
+		template<typename S1, typename S2 = std::string,
+			typename = std::enable_if_t<!std::is_constructible_v<std::decay_t<S1>, Customer>>>
+			Customer(S1&& s1, S2&& s2 = "", int i = 0)
+			:first(std::forward<S1>(s1)), second(std::forward<S2>(s2)), id(i)
+		{}
+	};
+
+	template<int N, typename = std::enable_if_t< N >= 0>>
+	struct factorial
+	{
+		static const int value = N * factorial<N - 1>::value;
+	};
+
+	template<>
+	struct factorial<7> {
+		static const int value = 1;
+	};
 }
--- Template Metaprogramming/main.cpp
+++ Template Metaprogramming/main.cpp
@@ -4,14 +4,33 @@
 #include <vector>
 #include <chrono>
 
-template<typename T>
-class A;
+class Customer {
+	std::string first;
+	std::string second;
+	int id;
+public:
+	template<typename S1, typename S2 = std::string,
+		typename = std::enable_if_t<!std::is_constructible_v<std::decay_t<S1>,Customer>>>
+	Customer(S1&& s1, S2&& s2 = "", int i = 0)
+		:first(std::forward<S1>(s1)), second(std::forward<S2>(s2)),id(i)
+	{}
+};
 
-template<typename T>
-void print(T t) {}
+template<int N, typename = std::enable_if_t< N >= 0>>
+struct factorial 
+{
+	static const int value = N * factorial<N - 1>::value;	
+};
+
+template<>
+struct factorial<7> {
+	static const int value = 1;
+};
 
 int main()
 {
-    int a=0;
-    A<decltype(a)> obj;
+	const Customer c("Edo");
+	Customer f(c);
+
+	std::cout << factorial<8>::value << std::endl;
 }
